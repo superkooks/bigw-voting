9679f8edcd9752620c75376a41f9a0676eca081c
SuperKooks <superkooks@superkooks.com>
2020-11-11 08:01:42 +1100

    Fix formatting of tf script

--- digitalocean.tf
+++ digitalocean.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "1.22.2"
     }
   }
@@ -27,50 +27,50 @@ data "digitalocean_ssh_key" "Vetinari" {
 
 
 resource "digitalocean_droplet" "bigwpeer" {
-    count = var.vm_count
+  count = var.vm_count
 
-    image = "ubuntu-20-04-x64"
-    name = "bigwpeer-${count.index}"
-    region = "sgp1"
-    size = "s-1vcpu-1gb"
-    private_networking = true
-    ssh_keys = [
-        data.digitalocean_ssh_key.UnseenUniversity.id,
-        data.digitalocean_ssh_key.Vetinari.id
+  image              = "ubuntu-20-04-x64"
+  name               = "bigwpeer-${count.index}"
+  region             = "sgp1"
+  size               = "s-1vcpu-1gb"
+  private_networking = true
+  ssh_keys = [
+    data.digitalocean_ssh_key.UnseenUniversity.id,
+    data.digitalocean_ssh_key.Vetinari.id
+  ]
+
+  connection {
+    host    = self.ipv4_address
+    user    = "root"
+    type    = "ssh"
+    agent   = true
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+
+      # Install golang
+      "wget https://golang.org/dl/go1.15.3.linux-amd64.tar.gz",
+      "tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz",
+      "export PATH=$PATH:/usr/local/go/bin",
+
+      # Setup env, download code
+      "export GOPATH=$HOME/go/",
+      "echo \"GOPATH=$HOME/go/\" >> ~/.bashrc",
+      "echo \"PATH=$PATH:/usr/bin\" >> ~/.bashrc",
+      "mkdir go",
+      "cd go",
+      "mkdir src",
+      "cd src",
+      "git clone https://github.com/SuperKooks/bigw-voting.git",
+      "cd bigw-voting",
+      "go mod download all"
     ]
-
-    connection {
-        host = self.ipv4_address
-        user = "root"
-        type = "ssh"
-        agent = true
-        timeout = "2m"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "export PATH=$PATH:/usr/bin",
-
-            # Install golang
-            "wget https://golang.org/dl/go1.15.3.linux-amd64.tar.gz",
-            "tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz",
-            "export PATH=$PATH:/usr/local/go/bin",
-
-            # Setup env, download code
-            "export GOPATH=$HOME/go/",
-            "echo \"GOPATH=$HOME/go/\" >> ~/.bashrc",
-            "echo \"PATH=$PATH:/usr/bin\" >> ~/.bashrc",
-            "mkdir go",
-            "cd go",
-            "mkdir src",
-            "cd src",
-            "git clone https://github.com/SuperKooks/bigw-voting.git",
-            "cd bigw-voting",
-            "go mod download all"
-        ]
-    }
+  }
 }
 
 output "ip" {
-    value = join("\n", digitalocean_droplet.bigwpeer[*].ipv4_address)
+  value = join("\n", digitalocean_droplet.bigwpeer[*].ipv4_address)
 }
