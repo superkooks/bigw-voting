b228e300b8d5fe5c552aed49569f62cfbb25f68a
SuperKooks <superkooks@superkooks.com>
2021-01-07 09:01:45 +1100

    Fix nick command only taking the first argument

--- commands/nick.go
+++ commands/nick.go
@@ -3,11 +3,12 @@ package commands
 import (
 	"bigw-voting/p2p"
 	"bigw-voting/util"
+	"strings"
 )
 
 // CommandNick changes the nickname shown by other peers
 func CommandNick(args []string) {
-	err := p2p.BroadcastMessage([]byte("Nick "+args[0]), 0)
+	err := p2p.BroadcastMessage([]byte("Nick "+strings.Join(args, " ")), 0)
 	if err != nil {
 		util.Errorln(err)
 		return
